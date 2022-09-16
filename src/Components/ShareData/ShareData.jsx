09d37bff78dae0085e4da43f48e8f47bd9cfb779
Valentinova Yuliya Olegovna <yvalentinova@ozon.ru>
2022-09-16 21:26:42 +0300

    New data

--- src/Components/ShareData/ShareData.jsx
+++ src/Components/ShareData/ShareData.jsx
@@ -1,73 +1,80 @@
 import React, { Component } from "react";
-import { Stack, IconButton } from '@mui/material';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import BloodtypeIcon from '@mui/icons-material/Bloodtype';
-import ThermostatIcon from '@mui/icons-material/Thermostat';
-import AddIcon from '@mui/icons-material/Add';
-import VaccinesIcon from '@mui/icons-material/Vaccines';
-import { Header } from "./../Header";
+import { Stack, IconButton, Grid, Button } from "@mui/material";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import BloodtypeIcon from "@mui/icons-material/Bloodtype";
+import ThermostatIcon from "@mui/icons-material/Thermostat";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import VaccinesIcon from "@mui/icons-material/Vaccines";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
 
-export class ShareData extends Component {
-    render() {
-        return (
-          <div>
-            <Header />
-            <h1>My data</h1>
-            <Stack
-              direction="column"
-              justifyContent="center"
-              alignItems="flex-start"
-              spacing={1}
+export function ShareData() {
+  return (
+    <div>
+      <h1>My data</h1>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="flex-start"
+        spacing={1}
+      >
+        <Grid container spacing={1} justifyContent="center">
+          <Grid item xs={9}>
+            <Paper
+              component="form"
+              sx={{
+                p: "2px 4px",
+                display: "flex",
+                alignItems: "center",
+              }}
             >
-              <List
-                sx={{
-                  width: "100%",
-                  maxWidth: 360,
-                  bgcolor: "background.paper",
-                }}
-              >
-                <ListItem>
-                  <ListItemAvatar>
-                    <Avatar>
-                      <BloodtypeIcon />
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText primary="Blood" secondary="Jan 9, 2022" />
-                </ListItem>
-                <ListItem>
-                  <ListItemAvatar>
-                    <Avatar>
-                      <ThermostatIcon />
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary="Body temperature"
-                    secondary="Jan 7, 2022"
-                  />
-                </ListItem>
-                <ListItem>
-                  <ListItemAvatar>
-                    <Avatar>
-                      <VaccinesIcon />
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText
-                    primary="Vaccination info"
-                    secondary="July 20, 2022"
-                  />
-                </ListItem>
-                <ListItem>
-                  <IconButton color="primary" aria-label="add to shopping cart" size="large">
-                    <AddIcon />
-                  </IconButton>
-                </ListItem>
-              </List>
+              <IconButton type="button" sx={{ p: "8px" }} aria-label="search">
+                <SearchIcon />
+              </IconButton>
+              <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search data..." />
+            </Paper>
+          </Grid>
+          <Grid item xs={3}>
+            <Button
+              variant="contained"
+              startIcon={<AddIcon />}
+              size="large"
+              sx={{ width: 180 }}
+            >
+              Add
+            </Button>
+          </Grid>
+        </Grid>
+        <Stack
+          direction="column"
+          justifyContent="center"
+          alignItems="flex-start"
+          spacing={2}
+        >
+          <Paper sx={{ width: 1290 }}>
+            <Stack direction="row" alignItems="center" justifyContent="center">
+              <ListItemAvatar sx={{ margin: 3 }}>
+                <Avatar>
+                  <VaccinesIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary="Vaccination info"
+                secondary="July 20, 2022"
+              />
+              <IconButton color="primary" size="large" sx={{ margin: 3 }}>
+                <DeleteIcon />
+              </IconButton>
             </Stack>
-          </div>
-        );
-    }
+          </Paper>
+        </Stack>
+      </Stack>
+    </div>
+  );
 }
