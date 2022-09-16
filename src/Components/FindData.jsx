85bcdb947c3a7237aa40cf6f3aa9d6effda57772
Elizaveta Bezlutskaya <ebezlutskaya@ozon.ru>
2022-09-16 16:32:31 +0200

    icons

--- src/Components/FindData.jsx
+++ src/Components/FindData.jsx
@@ -14,11 +14,20 @@ export class FindData extends Component {
     render() {
         return(
             <>
-                <Header/>
+                {/* <Header/> */}
                 <div className="container">
-                    <h2>My explore</h2>
-                    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                        {["Blood", "Result1", "Result2"].map((value) => (
+                    My explore
+                    <List sx={{ 
+                        width: '100%',
+                        maxWidth: 360, 
+                        bgcolor: 'background.paper',
+                        maxHeight: 300,
+                        overflow: 'auto',
+                        }}
+                    >
+                        {["Blood", "Result1", "Result2","Blood", "Result1", "Result2",
+                          "Blood", "Result1", "Result2",
+                          "Blood", "Result1", "Result2"].map((value) => (
                             <ListItem
                                 key={value}
                                 disableGutters
@@ -26,7 +35,7 @@ export class FindData extends Component {
                                     <IconButton edge="end" aria-label="delete">
                                       <SaveAltIcon />
                                     </IconButton>
-                                  }
+                                }
                             >
                                 
                                 <ListItemText primary={`${value}`}/>
@@ -36,7 +45,9 @@ export class FindData extends Component {
                     </List>
                     <Button
                         href="/find-data/create-study"
-                    > Create study </Button>
+                    > 
+                        Create study 
+                    </Button>
                 </div>
             </>
         )
