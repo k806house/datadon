e2d8f492cdedaa4a2da01c8bb8b409582cd33bd4
Elizaveta Bezlutskaya <ebezlutskaya@ozon.ru>
2022-09-17 17:37:15 +0200

    users

--- src/Components/SelectUser.jsx
+++ src/Components/SelectUser.jsx
@@ -1,39 +1,66 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import {Box, Stack, Avatar} from '@mui/material';
-import FormControl from '@mui/material/FormControl';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormLabel from '@mui/material/FormLabel';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import Switch from '@mui/material/Switch';
-import SpeedDial from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
-import SpeedDialAction from '@mui/material/SpeedDialAction';
-import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import PrintIcon from '@mui/icons-material/Print';
-import ShareIcon from '@mui/icons-material/Share';
+import {React, useState} from "react";
+import { styled } from "@mui/material/styles";
+import { Stack, Avatar, Button, Image } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormLabel from "@mui/material/FormLabel";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import Switch from "@mui/material/Switch";
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
+import FileCopyIcon from "@mui/icons-material/FileCopyOutlined";
+import SaveIcon from "@mui/icons-material/Save";
+import PrintIcon from "@mui/icons-material/Print";
+import ShareIcon from "@mui/icons-material/Share";
 
-import photo from '../assets/test.jpg';
+import photo from "../assets/test.jpg";
 
-import axios from 'axios';
+import axios from "axios";
 
+import "./SelectUser.css"
 
+export function SelectUser() {
+    const [data, setData] = useState({
+        value: '',
+        loading: true,
+    });
 
-export async function SelectUser() {
-    const response = await axios.post(
-        'https://01rtunofc9.execute-api.eu-west-1.amazonaws.com/serverless_lambda_stage/user/get'
-      );
-    console.log(response.data);
-    return (
-        <div>
-            <Stack direction="row" spacing={2}>
-                <Avatar alt="Remy Sharp" />
-                <Avatar alt="Travis Howard" src="../assets/test.jpg" />
-                <Avatar alt="Cindy Baker" src="../assets/test.jpg" />
-                <img src="../assets/test.jpg"></img>
-            </Stack>
-        </div>
-    );
+    var users;
+    axios.post(
+      'https://01rtunofc9.execute-api.eu-west-1.amazonaws.com/serverless_lambda_stage/user/get'
+    ).then( function (response) {
+            setData({
+                users: response.data.user,
+                loading: false,
+            })
+            console.log(response.data.user);
+    });
+  return (
+    <div>
+      <Stack
+        component="span"
+        justifyContent="center"
+        alignItems="center"
+        direction="row"
+        spacing={2}
+        sx={{}}
+      >
+        {!data.loading
+          ? data.users.map((d, i) => (
+            <div className="">
+                {" "}
+                <a
+                 href={d.photo_url}
+                >
+                <img src={d.photo_url} alt="..." className="user-image"/>
+                <h4>{d.first_name}</h4>
+                </a>
+            </div>
+            ))
+          : "loading"}
+      </Stack>
+    </div>
+  );
 }
