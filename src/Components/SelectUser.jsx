02b00e79f9f949003331fb9f05e1c3931a1e30ff
Elizaveta Bezlutskaya <ebezlutskaya@ozon.ru>
2022-09-17 18:16:30 +0200

    users

--- src/Components/SelectUser.jsx
+++ src/Components/SelectUser.jsx
@@ -1,4 +1,4 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 import { styled } from "@mui/material/styles";
 import { Stack, Avatar, Button, Image } from "@mui/material";
 import FormControl from "@mui/material/FormControl";
@@ -19,49 +19,53 @@ import photo from "../assets/test.jpg";
 
 import axios from "axios";
 
-import "./SelectUser.css"
+import "./SelectUser.css";
 
 export function SelectUser() {
-    const [data, setData] = useState({
-        value: '',
-        loading: true,
-    });
+  const [data, setData] = useState({
+    value: "",
+    loading: true,
+  });
 
-    var users;
-    axios.post(
-      'https://01rtunofc9.execute-api.eu-west-1.amazonaws.com/serverless_lambda_stage/user/get'
-    ).then( function (response) {
-            setData({
-                users: response.data.user,
-                loading: false,
-            })
-            console.log(response.data.user);
+  var users;
+  axios
+    .post(
+      "https://01rtunofc9.execute-api.eu-west-1.amazonaws.com/serverless_lambda_stage/user/get"
+    )
+    .then(function (response) {
+      setData({
+        users: response.data.user,
+        loading: false,
+      });
+      console.log(response.data.user);
     });
   return (
     <div>
-      <Stack
-        component="span"
-        justifyContent="center"
-        alignItems="center"
-        direction="row"
-        spacing={2}
-        sx={{ p: 30}}
+      <Stack 
+        spacing={4}
+        className="wrap"
       >
-        {!data.loading
-          ? data.users.map((d, i) => (
-            <div className="">
-                {" "}
-                <a
-                 href="/find-data"
-                >
-                    <img src={d.photo_url} className="user-image"/>
-                </a>
-                {/* <div className="caption"> */}
-                <h4>{d.first_name}</h4>
-                {/* </div> */}
-            </div>
-            ))
-          : "loading"}
+        <h2> Select user </h2>
+        <Stack
+          component="span"
+          justifyContent="center"
+          alignItems="center"
+          direction="row"
+          spacing={2}
+          sx={{}}
+        >
+          {!data.loading
+            ? data.users.map((d, i) => (
+                <div className="">
+                  {" "}
+                  <a href="/find-data">
+                    <img src={d.photo_url} className="user-image" />
+                  </a>
+                  <h4>{d.first_name}</h4>
+                </div>
+              ))
+            : "loading"}
+        </Stack>
       </Stack>
     </div>
   );
